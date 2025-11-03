4fee673c8c85e8cdc3ac7629d594098c15a91409
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2025-11-03 19:27:33 +0100

    Fixed test no longer useful

---
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/test-playwright/e2e/LoginRender.spec.ts
@@ -0,0 +1,12 @@
+﻿import { test, expect } from '@playwright/test'
+
+
+test('redirige al login y muestra el título', async ({ page }) => {
+  await page.goto('/');
+
+  // Verifica que se muestra el título de la pantalla de login
+  await expect(page.locator('h2')).toHaveText('Inicio sesión');
+
+  // Verifica que aparece el estado "No conectado"
+  await expect(page.locator('text=No conectado')).toBeVisible();
+});
