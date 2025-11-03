ba835d4ded68b574af6ffc838ad05248d04ee7c5
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2025-11-03 18:36:37 +0100

    Commented out initial test because it is no longer useful

--- Code/3DMANAGER-APP/3dmanager-app.client/src/test/unitary/printerList.test.tsx
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/test/unitary/printerList.test.tsx
@@ -1,7 +1,12 @@
+//Comento este test por que deja de tener sentido una vez comenzado el proyecto real
+/*
 import { render, screen } from '@testing-library/react'
 import App from '../../App'
+
+
 
 test('renders app title', () => {
     render(<App />)
     expect(screen.getByText(/3D Manager/i)).toBeInTheDocument()
 })
+*/
