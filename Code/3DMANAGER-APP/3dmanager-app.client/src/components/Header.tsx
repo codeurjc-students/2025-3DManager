3ce99ad86392391edf4041121340c0e78a41e21e
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2025-11-08 11:14:59 +0100

    Add logout redirection

--- Code/3DMANAGER-APP/3dmanager-app.client/src/components/Header.tsx
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
-
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
     const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogoutClick = () => {
+        logout();
+        navigate("/login");
+    };
 
     return (
         <header className="header-container">
@@ -15,7 +21,7 @@ const Header: React.FC = () => {
                 {user ? (
                     <>
                         <span className="me-3 ">Hola, {user.userName}</span>
-                        <button className="btn btn-outline-light btn-sm" onClick={logout}>
+                        <button className="btn btn-outline-light btn-sm" onClick={handleLogoutClick}>
                             Logout
                         </button>
                     </>
