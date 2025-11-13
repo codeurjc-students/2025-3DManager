c89597c16d220ec521c69b5a004781290e191fb8
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2025-11-13 21:03:17 +0100

    Deleted class wrongly named

--- Code/3DMANAGER-APP/3DMANAGER-APP.BLL/Models/Group/GroupRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _3DMANAGER_APP.BLL.Models.Group
-{
-    public class GroupRequest
-    {
-        public string UserId { get; set; }
-        public string GroupName { get; set; }
-        public string GroupDescription { get; set; }
-    }
-}
