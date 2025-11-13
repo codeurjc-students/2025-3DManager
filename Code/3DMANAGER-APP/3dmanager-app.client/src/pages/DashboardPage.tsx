d77e275a08e31461b9a21efe7a72eef8f289050e
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2025-11-13 23:07:12 +0100

    Base dashboard layout for beggining

--- Code/3DMANAGER-APP/3dmanager-app.client/src/pages/DashboardPage.tsx
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/pages/DashboardPage.tsx
@@ -2,11 +2,18 @@
 
 const DashboardPage: React.FC = () => {
 
-
-
     return (
-        
-       <div></div> 
+        <div id="dashboard d-flex">
+            <div className="grey-container col-6 vh-100">
+                <h2 className="title-impact-2 ms-3 mt-4">Impresoras</h2>
+                <hr className="m-3"></hr>
+            </div>
+            <div className="white-container col-6 vh-100">
+                <h2 className="title-impact-2 ms-3 mt-4">Datos Generales</h2>
+                <hr className ="m-3"></hr>
+            </div>
+        </div> 
     );
+
 };
 export default DashboardPage;
