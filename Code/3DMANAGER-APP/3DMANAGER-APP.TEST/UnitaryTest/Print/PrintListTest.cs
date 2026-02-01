7d84eade2f1888c2814a521392c977a6b56a496e
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2026-02-01 22:56:06 +0100

    Test adaptation to changes on server side pagination on prints list

--- Code/3DMANAGER-APP/3DMANAGER-APP.TEST/UnitaryTest/Print/PrintListTest.cs
+++ Code/3DMANAGER-APP/3DMANAGER-APP.TEST/UnitaryTest/Print/PrintListTest.cs
@@ -38,35 +38,36 @@ namespace _3DMANAGER_APP.TEST.UnitaryTest.Print
             int groupId = 1;
 
             var dbResponse = new List<PrintListResponseDbObject>
-    {
-        new PrintListResponseDbObject
-        {
-            PrintId = 1,
-            PrintName = "Test Print 1",
-            PrintUserCreator = "user1",
-            PrintDate = new DateTime(2024, 1, 1),
-            PrintTime = 3720, // 1h 2min
-            PrintFilamentConsumed = 12.5m
-        },
-        new PrintListResponseDbObject
-        {
-            PrintId = 2,
-            PrintName = "Test Print 2",
-            PrintUserCreator = "user2",
-            PrintDate = new DateTime(2024, 1, 2),
-            PrintTime = 780, // 0h 13min
-            PrintFilamentConsumed = 5.3m
-        }
-    };
+            {
+                new PrintListResponseDbObject
+                {
+                    PrintId = 1,
+                    PrintName = "Test Print 1",
+                    PrintUserCreator = "user1",
+                    PrintDate = new DateTime(2024, 1, 1),
+                    PrintTime = 3720, // 1h 2min
+                    PrintFilamentConsumed = 12.5m
+                },
+                new PrintListResponseDbObject
+                {
+                    PrintId = 2,
+                    PrintName = "Test Print 2",
+                    PrintUserCreator = "user2",
+                    PrintDate = new DateTime(2024, 1, 2),
+                    PrintTime = 780, // 0h 13min
+                    PrintFilamentConsumed = 5.3m
+                }
+            };
 
+            int totalItems = 0;
             _printDbManagerMock
-                .Setup(db => db.GetPrintList(groupId))
+                .Setup(db => db.GetPrintList(groupId, 1, 10, out totalItems))
                 .Returns(dbResponse);
 
 
             var mapperConfig = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<PrintListResponseDbObject, PrintListResponse>()
+                cfg.CreateMap<PrintListResponseDbObject, PrintResponse>()
                    .ForMember(dest => dest.PrintTime,
                        opt => opt.MapFrom(src =>
                            $"{(int)TimeSpan.FromSeconds((double)src.PrintTime).TotalHours}h " +
@@ -81,18 +82,18 @@ namespace _3DMANAGER_APP.TEST.UnitaryTest.Print
                 _loggerMock.Object
             );
 
-
-            var result = manager.GetPrintList(groupId, out BaseError? error);
+            PagedRequest pagedRequest = new PagedRequest { PageNumber = 1, PageSize = 10 };
+            var result = manager.GetPrintList(groupId, pagedRequest, out BaseError? error);
 
 
             Assert.NotNull(result);
-            Assert.Equal(2, result.Count);
+            Assert.Equal(2, result.prints.Count);
             Assert.Null(error);
 
-            Assert.Equal("1h 2min", result[0].PrintTime);
-            Assert.Equal("0h 13min", result[1].PrintTime);
+            Assert.Equal("1h 2min", result.prints[0].PrintTime);
+            Assert.Equal("0h 13min", result.prints[1].PrintTime);
 
-            _printDbManagerMock.Verify(db => db.GetPrintList(groupId), Times.Once);
+            _printDbManagerMock.Verify(db => db.GetPrintList(groupId, 1, 10, out totalItems), Times.Once);
         }
     }
 }
