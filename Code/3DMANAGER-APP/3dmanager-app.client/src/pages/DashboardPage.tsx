c27a5730098b1a57184bb53b2057acbdaff2a319
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2026-04-14 00:13:19 +0200

    Added padding to visualize better the printer list

--- Code/3DMANAGER-APP/3dmanager-app.client/src/pages/DashboardPage.tsx
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/pages/DashboardPage.tsx
@@ -48,7 +48,7 @@ const DashboardPage: React.FC = () => {
 
     return (
         <div id="dashboard" className="d-flex vh-100">
-            <div className="grey-container col-6 vh-100 scroll-container">
+            <div className="grey-container col-6 pb-5 vh-100 scroll-container">
                 <h2 className="title-impact-2 ms-3 mt-4">Impresoras</h2>
                 <hr className="m-2"></hr>
                 <div>
