0fe22dc809da2961e8cc502ddf9ae5752b140a8f
ivicenter2018 <i.vicenter.2018@alumnos.urjc.es>
2026-04-18 15:54:29 +0200

    delete functionality not used

--- Code/3DMANAGER-APP/3dmanager-app.client/src/pages/PrintDetailsPage.tsx
+++ Code/3DMANAGER-APP/3dmanager-app.client/src/pages/PrintDetailsPage.tsx
@@ -50,13 +50,6 @@ const PrintDetailPage: React.FC = () => {
         return hours * 3600 + minutes * 60;
     };
 
-    const formatSeconds = (seconds: number): string => {
-        const h = Math.floor(seconds / 3600);
-        const m = Math.floor((seconds % 3600) / 60);
-        return `${h}h ${m}min`;
-    };
-
-
     const handleUpdate = async () => {
     try {
         const seconds = parseTimeToSeconds(realTime);
